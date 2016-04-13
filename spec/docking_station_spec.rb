72701fb668b3d03aa2087f8a2dff5d38918ef370
Mara Wanot <marawanot@Maras-MacBook-Pro.local>
2016-04-13 11:04:18 +0100

    DockingStation edited test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,6 +11,10 @@ require "docking_station"
 describe DockingStation do
   describe "release bike" do
   	it{should respond_to(:release_bike)}
+  	it "releases a working bike" do
+  	  bike = subject.release_bike
+  	  expect(bike).to be_working
+  	end
 
     it 'is a class of Bike' do
       expect(subject.release_bike).to be_an_instance_of Bike
