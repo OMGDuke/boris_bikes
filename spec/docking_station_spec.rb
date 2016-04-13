22dbef610572944005bed59b0deb7f211d932aa5
Mara Wanot <m.d.wanot@hotmail.com>
2016-04-13 12:36:40 +0100

    Releasing a bike no longer created a new bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,25 +1,20 @@
-# require "docking_station"
-
-# describe DockingStation do
-#   it "releases bike" do
-#     expect(release_bike).to eq "Bike released"
-#   end
-# end
-
 require "docking_station"
 
 describe DockingStation do
   describe "release bike" do
   	it{should respond_to(:release_bike)}
   	it "releases a working bike" do
-  	  bike = subject.release_bike
-  	  expect(bike).to be_working
+  	  bike = Bike.new
+  	  subject.dock(bike)
+  	  expect(subject.release_bike).to be_working
   	end
     it "raises an error when no bikes are available" do
       expect{subject.release_bike}.to raise_error
     end
 
     it 'is a class of Bike' do
+      bike = Bike.new
+  	  subject.dock(bike)
       expect(subject.release_bike).to be_an_instance_of Bike
     end
   end
