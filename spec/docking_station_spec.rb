ccf42bec74b6069075cd84c5c50f73e568894f1d
Matthew Ward <matthewward@Matthews-MacBook-Pro-2.local>
2016-04-13 13:43:00 +0100

    Updated tests to include let

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,17 @@
   require 'docking_station'
   describe DockingStation do
 
+    let(:bike) { Bike.new }
+    let(:station) { DockingStation.new }
+    let(:docked_bike) { station.dock(bike) }
+
     describe '#release_bike' do
       it 'releases a bike' do
-        bike = Bike.new
         subject.dock(bike)
         expect(subject.release_bike).to eq bike
       end
 
       it 'releases working bikes' do
-        bike = Bike.new
         expect(bike).to be_working
       end
     end
@@ -22,18 +24,13 @@
     it { is_expected.to respond_to(:dock).with(1).argument}
 
     it 'allows a user to see a docked bike' do
-      station = DockingStation.new
-      bike = Bike.new
-      docked_bike = station.dock(bike)
       expect(station.bikes).to eq docked_bike
     end
 
     it 'gives an error message when there is no bike' do
-      station = DockingStation.new
       expect { station.release_bike }.to raise_error("All bikes taken")
     end
     it 'gives an error message there are no spaces to dock' do
-      station = DockingStation.new
       20.times { station.dock(Bike.new) }
       expect { station.dock(Bike.new) }.to raise_error("Full!")
     end
