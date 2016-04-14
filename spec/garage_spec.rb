bf67537e34a8751c5312c83cdb65a25aa1f9a73d
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-14 17:46:09 +0100

    updated garage spec

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,7 +1,5 @@
 require 'garage'
 describe Garage do
-  let(:bike) {bike=double("bike")}
-  let(:fixMethod) {allow(bike).to receive(:fix) {true}}
   # bike = double("bike")
   it 'starts a garage with storage space for fixed bikes' do
     expect(subject.fixed_bikes).to eq []
@@ -13,19 +11,15 @@ describe Garage do
     it 'can move bikes' do
       bike=double("bike")
       subject.garage_broken_bikes=([bike])
-        allow(bike).to receive(:fix) {true}
       subject.fix_bikes
       expect(subject.fixed_bikes).to eq [bike]
     end
 
     it 'removes bikes from garage_broken_bikes' do
-    bike #created a let at the top of the page
-
+      bike=double("bike")
       subject.garage_broken_bikes=([bike])
-
-    fixMethod #sure this is wrong but ive allowed the bike double to recieve a fix method!
-
-       subject.fix_bikes
+      expect(subject.fix).to eq true
+      subject.fix_bikes
       expect(subject.garage_broken_bikes).to eq []
     end
 
